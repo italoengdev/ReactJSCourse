5fe03df6e241d64096178d13b86662328fca0cbd
italoengdev <italo_saraiva182@hotmail.com>
2022-12-08 00:39:18 -0300

    RequestApiState

--- src/App.js
+++ src/App.js
@@ -4,46 +4,62 @@ import { Component } from 'react'
 class App extends Component {
   //class fields subs constructor and super
   state = {
-    counter: 0,
     posts: [
-      {
-        id: 1,
-        title: 'O título 1',
-        body: 'Corpo 1'
-      },
-      {
-        id: 2,
-        title: 'O título 2',
-        body: 'Corpo 2'
-      },
-      {
-        id: 3,
-        title: 'O título 3',
-        body: 'Corpo 3'
-      }
+      // {
+      //   id: 1,
+      //   title: 'O título 1',
+      //   body: 'Corpo 1'
+      // },
+      // {
+      //   id: 2,
+      //   title: 'O título 2',
+      //   body: 'Corpo 2'
+      // },
+      // {
+      //   id: 3,
+      //   title: 'O título 3',
+      //   body: 'Corpo 3'
+      // }
     ]
   }
-  timeoutUpdate = null
+  // timeoutUpdate = null
 
-  handleTimeOut = () => {
-    const { posts, counter } = this.state
-    posts[0].title = 'O título mudou'
+  // handleTimeOut = () => {
+  //   const { posts, counter } = this.state
+  //   posts[0].title = 'O título mudou'
 
-    this.timeoutUpdate = setTimeout(() => {
-      this.setState({ posts, counter: counter + 1 })
-    }, 1000)
-  }
+  //   this.timeoutUpdate = setTimeout(() => {
+  //     this.setState({ posts, counter: counter + 1 })
+  //   }, 1000)
+  // }
   //lifecycles methods React
   componentDidMount() {
-    this.handleTimeOut()
+    this.loadPosts()
+    // this.handleTimeOut()
+  }
+
+  loadPosts = async () => {
+    const postResponse = fetch("https://jsonplaceholder.typicode.com/posts")
+    const photoResponse = fetch("https://jsonplaceholder.typicode.com/photos")
+
+    const [posts, photos] = await Promise.all([postResponse, photoResponse])
+
+    const postsJson = await posts.json()
+    const photosJson = await photos.json()
+
+    const postsAndPhotos = postsJson.map((post,index) => {
+      return { ...post, cover: photosJson[index].url}
+    })
+
+    this.setState({posts: postsAndPhotos})
   }
 
   componentDidUpdate() {
-    this.handleTimeOut()
+    // this.handleTimeOut()
   }
   //limpar lixo gerado na renderização
   componentWillUnmount(){
-    clearTimeout(this.timeoutUpdate)
+    // clearTimeout(this.timeoutUpdate)
   }
   //Aula01
   // handlePClick = () => {
@@ -57,37 +73,24 @@ class App extends Component {
   // }
 
   render() {
-    const { posts, counter } = this.state
+    const { posts} = this.state
     // or Destructiring const {name} = this.state / const name = this.state.name
 
     return (
-      <div className="App">
-        <h1>{counter}</h1>
+      <section className='container'>
+        <div className="posts">
         {posts.map(post => (
-          <div key={post.id}>
-            <h1>{post.title}</h1>
-            <h3>{post.body}</h3>
+          <div className='post'>
+            <img src={post.cover} alt={post.title}/>
+            <div key={post.id} className="post-content">
+              <h1>{post.title}</h1>
+              <h3>{post.body}</h3>
+            </div>
           </div>
         ))}
-        {
-          //Aula 01
-          /* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p onClick={this.handlePClick}>
-            {name} {counter}
-          </p>
-          <a
-            onClick={this.handleAClick}
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Contador
-          </a>
-        </header> */
-        }
-      </div>
+        </div>
+      </section>
+      
     )
   }
 }
