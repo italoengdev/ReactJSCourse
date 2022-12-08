699b5a96ff3ab992c5f27c7004cf675e058bdd6d
italoengdev <italo_saraiva182@hotmail.com>
2022-12-07 23:37:22 -0300

    lifecycles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { Component } from 'react'
 
-function App() {
+class App extends Component {
+  //class fields subs constructor and super
+  state = {
+    counter: 0,
+    posts: [
+      {
+        id: 1,
+        title: 'O título 1',
+        body: 'Corpo 1'
+      },
+      {
+        id: 2,
+        title: 'O título 2',
+        body: 'Corpo 2'
+      },
+      {
+        id: 3,
+        title: 'O título 3',
+        body: 'Corpo 3'
+      }
+    ]
+  }
+  timeoutUpdate = null
+
+  handleTimeOut = () => {
+    const { posts, counter } = this.state
+    posts[0].title = 'O título mudou'
+
+    this.timeoutUpdate = setTimeout(() => {
+      this.setState({ posts, counter: counter + 1 })
+    }, 1000)
+  }
+  //lifecycles methods React
+  componentDidMount() {
+    this.handleTimeOut()
+  }
+
+  componentDidUpdate() {
+    this.handleTimeOut()
+  }
+  //limpar lixo gerado na renderização
+  componentWillUnmount(){
+    clearTimeout(this.timeoutUpdate)
+  }
+  //Aula01
+  // handlePClick = () => {
+  //   this.setState({ name: 'Senior' })
+  // }
+
+  // handleAClick = event => {
+  //   event.preventDefault()
+  //   const { counter } = this.state
+  //   this.setState({ counter: counter + 1 })
+  // }
+
+  render() {
+    const { posts, counter } = this.state
+    // or Destructiring const {name} = this.state / const name = this.state.name
+
+    return (
+      <div className="App">
+        <h1>{counter}</h1>
+        {posts.map(post => (
+          <div key={post.id}>
+            <h1>{post.title}</h1>
+            <h3>{post.body}</h3>
+          </div>
+        ))}
+        {
+          //Aula 01
+          /* <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p onClick={this.handlePClick}>
+            {name} {counter}
+          </p>
+          <a
+            onClick={this.handleAClick}
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Contador
+          </a>
+        </header> */
+        }
+      </div>
+    )
+  }
+}
+
+/* function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Olá Mundo.
         </p>
         <a
           className="App-link"
@@ -20,6 +111,6 @@ function App() {
       </header>
     </div>
   );
-}
+} */
 
-export default App;
+export default App
